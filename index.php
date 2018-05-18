88c3ff459ef6870589d594c9f6d25a439fb3d6c3
danushka96 <danushkaherath96@gmail.com>
2018-05-18 20:40:31 +0530

    fix paths

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 session_start();
 //--facebook login--
 set_include_path(get_include_path() . PATH_SEPARATOR . 'facebook-php-sdk\src');
-require_once 'Facebook\FacebookResponse.php';
-require_once 'Facebook\FacebookSession.php';
-require_once 'Facebook\FacebookSignedRequestFromInputHelper.php';
-require_once 'Facebook\FacebookRedirectLoginHelper.php';
-require_once 'Facebook\FacebookRequest.php';
-require_once 'Facebook\FacebookSDKException.php';
-require_once 'Facebook\FacebookRequestException.php';
-require_once 'Facebook\FacebookAuthorizationException.php';
-require_once 'Facebook\GraphObject.php';
+require_once ('Facebook/FacebookResponse.php');
+require_once ('Facebook/FacebookSession.php');
+require_once ('Facebook/FacebookSignedRequestFromInputHelper.php');
+require_once ('Facebook/FacebookRedirectLoginHelper.php');
+require_once ('Facebook/FacebookRequest.php');
+require_once ('Facebook/FacebookSDKException.php');
+require_once ('Facebook/FacebookRequestException.php');
+require_once ('Facebook/FacebookAuthorizationException.php');
+require_once ('Facebook/GraphObject.php');
 use Facebook\FacebookResponse;
 use Facebook\FacebookSession;
 use Facebook\FacebookSignedRequestFromInputHelper;
