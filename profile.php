c9869f26dd4b92518e0a0b622f6be571f8f3d576
danushka96 <danushkaherath96@gmail.com>
2018-05-18 21:36:28 +0530

    fix php redirect error

--- profile.php
+++ profile.php
@@ -66,6 +66,6 @@ if (isset($accessToken)) {
   	// Now you can redirect to another page and use the access token from $_SESSION['localhost_app_token']
 } else {
 	// replace your website URL same as added in the developers.facebook.com/apps e.g. if you used http instead of https and you used non-www version or www version of your website then you must add the same here
-	$loginUrl = $helper->getLoginUrl('https://facebooktrips.cf', $permissions);
+	$loginUrl = $helper->getLoginUrl('https://facebooktrips.cf/profile.php', $permissions);
 	echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
 }
